dd2d891bd1bdbfabcc51b3f11c164eb82008f0e4
Your Name <you@example.com>
2021-01-14 10:38:59 +0800

    add log println

--- solr-multiline.go
+++ solr-multiline.go
@@ -158,6 +158,7 @@ func GetContainerHostname(c *docker.Container) string {
 // Stream sends log data to the next adapter
 func (a *SolrAdapter) Stream(logstream chan *router.Message) { //nolint:gocyclo
 	for m := range logstream {
+		log.Println("stream:", m)
 		dockerInfo := DockerInfo{
 			Name:     m.Container.Name,
 			ID:       m.Container.ID,
@@ -209,6 +210,7 @@ func (a *SolrAdapter) Stream(logstream chan *router.Message) { //nolint:gocyclo
 			continue
 		}
 
+	       log.Println("data:", js)
 		// To work with tls and tcp transports via json_lines codec
 		js = append(js, byte('\n'))
 
